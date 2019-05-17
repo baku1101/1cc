fee366a81502e527bc6cc0c3b32eed64028df995
watanabe <kouki.1101@outlook.jp>
2019-05-17 15:30:56 +0900

    整数1つをコンパイルするコンパイラを作成(step1)

---
+++ step1/1cc.c
@@ -0,0 +1,18 @@
+// 引数で与えられた数をreturnする
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const* argv[])
+{
+	if (argc != 2) {
+		fprintf(stderr, "引数の個数が正しくない\n");
+		return 1;
+	}
+
+	printf(".intel_syntax noprefix\n");
+	printf(".global main\n");
+	printf("main:\n");
+	printf("  mov rax, %d\n", atoi(argv[1]));
+	printf("  ret\n");
+	return 0;
+}
